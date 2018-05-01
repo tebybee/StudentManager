4855995b0eb1f17d92c43dfb5dcfe4edb21a98d6
tebybee <tbybee@mail.csuchico.edu>
2018-05-01 11:46:57 -0700

    class file to allow functionality of main.cpp

---
+++ student.h
@@ -0,0 +1,21 @@
+#ifndef _STUDENT_H_
+#define _STDENT_H_
+#include <string>
+using namespace std;
+class student
+{
+private:
+String firstName;
+String lastName;
+public:
+void setName(String, String);
+{
+    
+}
+String fullName()
+{
+    
+}
+};
+#endif
+
